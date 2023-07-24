e59b68e43c39524c856c3a2ffa61d76a4c6ed552
narekhasratyan <hasratyan.2000@gmail.com>
2023-07-24 15:02:33 +0400

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo_welbex.svg";
+import "./App.scss";
+import { BiLogoTelegram, BiLogoWhatsapp } from "react-icons/bi";
+import { LiaPhoneVolumeSolid } from "react-icons/lia";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App px-[150px] font-bodyFont pb-[20px] pt-[46px] w-full h-auto bg-bodyColor text-lightText">
+      <header className="flex justify-between gap-[30px]">
+        <section className="flex gap-[52px]">
+          <div>
+            <img src={logo} alt="logo" />
+
+            <p className="font-light pt-[10px] block w-[165px] text-[10px] font-miniFont text-justify leading-[12.19px]">
+              крупный интегратор CRM <br />
+              в Росcии и ещё 8 странах
+            </p>
+          </div>
+          <div>
+            <ul>
+              <li className="flex gap-[30px] font-medium">
+                <a href="/#">Услуги</a>
+                <a href="/#">Выджеты</a>
+                <a href="/#">Интеграции</a>
+                <a href="/#">Кейсы</a>
+                <a href="/#">Сертификаты</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="flex gap-[47px]">
+          <div>
+            <p>+7 555 555-55-55</p>
+          </div>
+          <div className="flex gap-[30px]">
+            <a href="/#">
+              <BiLogoTelegram width={20} height={20} />
+            </a>
+            <a href="/#">
+              <BiLogoWhatsapp width={20} height={20} />
+            </a>
+            <a href="/#">
+              <LiaPhoneVolumeSolid width={20} height={20} />
+            </a>
+          </div>
+        </section>
       </header>
     </div>
   );
